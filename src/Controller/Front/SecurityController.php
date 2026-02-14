8d557fbaa37ee8ce5779e1fe4150986ab59de3a4
Idav <rimbault.david@gmail.com>
2026-02-14 09:12:47 +0100

    WIP. Front

---
+++ src/Controller/Front/SecurityController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/login')]
+    public function index(): Response
+    {
+        return $this->render('front/security/login.html.twig');
+    }
+
+    #[Route('/login_check', name: 'login_check')]
+    public function check(): never
+    {
+        throw new \LogicException('This code should never be reached');
+    }
+}
